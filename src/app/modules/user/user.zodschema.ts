1a38f61930f9594be29f4499372d8727920823fa
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-02 03:05:56 +0600

    [mod] : user.route.ts modified

--- src/app/modules/user/user.zodschema.ts
+++ src/app/modules/user/user.zodschema.ts
@@ -1,15 +1,12 @@
 import { z } from "zod";
 
 
-const userZodSchema= z.object({
-    id: z.string(),
+const userZodSchema = z.object({
     password: z
-        .string()
-        .max(20, { message: "password can be more than 20 characters" }),
-    needsPasswordChange: z.boolean().optional().default(true),
-    role: z.enum(["student", "faculty", "admin"]),
-    status: z.enum(["in-progress", "blocked"]).default("in-progress"),
-    isDeleted : z.boolean().optional().default(false)
+        .string({
+            invalid_type_error: 'Password must be string',
+        })
+        .max(20, { message: 'Password can not be more than 20 characters' })
+        .optional(),
 });
-
 export default userZodSchema;
