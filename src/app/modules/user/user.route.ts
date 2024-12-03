2cbeb979e259020f857ee4eaa50c8904a75fcf25
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-03 17:58:29 +0600

    [mod] : user.router.ts modified

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { UserControllers } from './user.controller';
+import validateRequest from '../../middlewares/validateRquest';
+import { createStudentValidationSchema } from '../student/student.zodSchema';
 
 const UserRouter: Router =Router();
 
-UserRouter.post('/create-student', UserControllers.createStudent);
+UserRouter.post(
+    '/create-student',
+    validateRequest(createStudentValidationSchema),
+    UserControllers.createStudent,
+);
 
 export default UserRouter;
