981089801b0de488879efd752b715d3a377bcb86
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-03 17:54:00 +0600

    [add] : validateRequest.ts middleware added

---
+++ src/app/middlewares/validateRquest.ts
@@ -0,0 +1,21 @@
+import type { NextFunction, Request, Response } from 'express';
+import type { AnyZodObject } from 'zod';
+
+/**
+ *
+ * @param schema A zod validation schema
+ * @returns A promise
+ */
+const validateRequest = (schema: AnyZodObject) => {
+    return async (req: Request, _res: Response, next: NextFunction) => {
+        try {
+            await schema.parseAsync(req.body);
+
+            next();
+        } catch (error) {
+            next(error);
+        }
+    };
+};
+
+export default validateRequest;
