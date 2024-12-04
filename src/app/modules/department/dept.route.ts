e57ab1e9e710f301a20deadb21d5de616b0cde34
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-04 20:49:41 +0600

    [add +mod] : get single department route added to dept.route.ts

--- src/app/modules/department/dept.route.ts
+++ src/app/modules/department/dept.route.ts
@@ -2,10 +2,14 @@ import { Router } from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { departmentCreationSchema } from "./dept.zodSchema";
 import { DepartmentControllers } from "./dept.controller";
+import { DepartmentServices } from "./dept.service";
 
 const DepartmentRouter: Router = Router();
 
 DepartmentRouter.post('/create-department', validateRequest(departmentCreationSchema), DepartmentControllers.createDepartment);
+
 DepartmentRouter.get('/',DepartmentControllers.getAllDepartment);
+
+DepartmentRouter.get("/:id", DepartmentServices.getSingleDepartment);
 
 export default DepartmentRouter;
