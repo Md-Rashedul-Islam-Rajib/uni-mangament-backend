20f00abcf8011e6b4a8bd8974041849fe9de8c7f
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-04 20:40:36 +0600

    [add +mod] : get all department controller added to dept.controller.ts

--- src/app/modules/department/dept.controller.ts
+++ src/app/modules/department/dept.controller.ts
@@ -8,4 +8,9 @@ export class DepartmentControllers {
         const result = await DepartmentServices.createDepartment(req.body);
         sendResponse(res,201,true,"Department created successfully", result);
     });
+
+    static getAllDepartment = catchAsync(async (req: Request, res: Response) => {
+        const result = await DepartmentServices.getAllDepartments();
+        sendResponse(res, 200, true, "Departments are retrieved successfully", result);
+    });
 }
