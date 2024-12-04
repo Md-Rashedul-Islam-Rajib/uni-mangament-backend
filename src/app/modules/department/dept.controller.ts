98d6585fd750730cc8ce985670162eb16e4f75d8
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-04 20:38:12 +0600

    [add] : dept.controller.ts added

---
+++ src/app/modules/department/dept.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import catchAsync from "../../utilities/catchAsyncFn";
+import { DepartmentServices } from "./dept.service";
+import sendResponse from "../../utilities/sendResponse";
+
+export class DepartmentControllers {
+    static createDepartment = catchAsync(async (req: Request, res: Response) => {
+        const result = await DepartmentServices.createDepartment(req.body);
+        sendResponse(res,201,true,"Department created successfully", result);
+    });
+}
