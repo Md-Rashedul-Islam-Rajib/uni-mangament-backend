43ad116bce6b8e4b72b5187e31203ee46a6274c2
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-04 20:15:00 +0600

    [add] : dept.model.ts added

---
+++ src/app/modules/department/dept.model.ts
@@ -0,0 +1,57 @@
+import { Department } from './dept.model';
+import { Schema, model } from 'mongoose';
+import { TDepartment } from './dept.types';
+
+
+const departmentSchema = new Schema<TDepartment>(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        code: {
+            type: String,
+            required: true,
+            uppercase: true,
+            unique: true,
+        },
+        faculty: {
+            type: Schema.Types.ObjectId,
+            ref: 'Faculty',
+        },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    },
+);
+
+departmentSchema.pre('save', async function (next) {
+    const departmentExists = await DepartmentModel.findOne({
+        name: this.name,
+    });
+
+    if (departmentExists) {
+        throw new Error(
+            ` Department with ${this.name} is already exists!`
+        );
+    }
+
+    next();
+});
+
+departmentSchema.pre('findOneAndUpdate', async function (next) {
+    const query = this.getQuery();
+    const departmentExists = await DepartmentModel.findOne(query);
+
+    if (!departmentExists) {
+        throw new Error(
+            'This department does not exist!'
+        );
+    }
+
+    next();
+});
+
+export const DepartmentModel = model<TDepartment>('Department', departmentSchema);
--- src/app/modules/department/dept.types.ts
+++ src/app/modules/department/dept.types.ts
@@ -3,5 +3,5 @@ import type { Types } from 'mongoose';
 export type TDepartment = {
     name: string;
     code: string;
-    Faculty: Types.ObjectId;
+    faculty: Types.ObjectId;
 };
