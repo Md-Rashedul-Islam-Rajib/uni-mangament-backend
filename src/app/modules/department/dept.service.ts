09711c971e20c7912eb8a5917080ae7144716957
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-04 20:26:55 +0600

    [add +mod] : create department service added to dept.service.ts

--- src/app/modules/department/dept.service.ts
+++ src/app/modules/department/dept.service.ts
@@ -1,6 +1,13 @@
 import { DepartmentModel } from "./dept.model";
+import { TDepartment } from "./dept.types";
 
 export class DepartmentServices {
+
+    static async createDepartment(payload: TDepartment) {
+        const result = await DepartmentModel.create(payload);
+        return result;
+    };
+
     static async getAllDepartments() {
         const result = await DepartmentModel.find().populate("faculty");
         return result;
