e089ff39cfc5f8820dcefb71ec261741963e3916
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-05 14:47:32 +0600

    [add + mod] : get all faculty added in faculty.controller.ts

--- src/app/modules/faculty/faculty.controller.ts
+++ src/app/modules/faculty/faculty.controller.ts
@@ -8,4 +8,12 @@ export class FacultyControllers {
         const result = await FacultyServices.createFaculty(req.body);
         sendResponse(res, 201, true, "Faculty is created successfully", result);
     });
+
+    static getAllFaculties = catchAsync(async (req: Request, res: Response) => {
+        const result = await FacultyServices.getAllFaculties();
+        sendResponse(res,200,true,"Faculties are retrieved successfully",result);
+    });
+
+    
+
 }
