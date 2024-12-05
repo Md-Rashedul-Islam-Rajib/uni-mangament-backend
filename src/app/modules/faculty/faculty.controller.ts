22f4734e3229900615d0335bef0f1b38aa4b5008
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-05 14:39:36 +0600

    [add] : faculty.controller.ts added

---
+++ src/app/modules/faculty/faculty.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+import catchAsync from "../../utilities/catchAsyncFn";
+import { FacultyServices } from "./faculty.service";
+import sendResponse from "../../utilities/sendResponse";
+
+export class FacultyControllers {
+    static createFaculty = catchAsync(async (req: Request, res: Response) => {
+        const result = await FacultyServices.createFaculty(req.body);
+        sendResponse(res, 201, true, "Faculty is created successfully", result);
+    });
+}
