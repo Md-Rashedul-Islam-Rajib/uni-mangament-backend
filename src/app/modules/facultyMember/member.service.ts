e24c8bec987d299e167bc970077429b189f04d5b
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-09 02:32:37 +0600

    [add + mod] : update faculty member added in member.service.ts

--- src/app/modules/facultyMember/member.service.ts
+++ src/app/modules/facultyMember/member.service.ts
@@ -1,6 +1,7 @@
 import QueryBuilder from "../../builder/queryBuilder";
 import { FacultyModel } from "../faculty/faculty.model";
 import { FacultySearchableFields } from "./member.constant";
+import { TFacultyMember } from "./member.types";
 
 export class FacultymemberServices {
     static async getAllFacultyMembers(query: Record<string, unknown>) {
@@ -22,6 +23,35 @@ export class FacultymemberServices {
     static async getSingleFacultyMember(id:string) {
         const result =
             await FacultyModel.findById(id).populate('academicDepartment');
+
+        return result;
+    }
+
+    static async updateFacultyMember(id: string, payload: Partial<TFacultyMember>) {
+        const { name, ...remainingFacultyData } = payload;
+
+        // Prepare the update object
+        const modifiedUpdatedData = {
+            ...remainingFacultyData,
+            ...(name &&
+                Object.keys(name).reduce(
+                    (acc, key) => ({
+                        ...acc,
+                        [`name.${key}`]: name[key as keyof typeof name],
+                    }),
+                    {},
+                )),
+        };
+
+        // Perform the database update
+        const result = await FacultyModel.findByIdAndUpdate(
+            id,
+            modifiedUpdatedData,
+            {
+                new: true,
+                runValidators: true,
+            },
+        );
 
         return result;
     }
