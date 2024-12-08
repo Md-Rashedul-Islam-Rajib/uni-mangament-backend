c541a19008164d69d07afcef00e3bbaea303c5b7
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-09 02:12:27 +0600

    [add] : member.service.ts added

---
+++ src/app/modules/facultyMember/member.service.ts
@@ -0,0 +1,21 @@
+import QueryBuilder from "../../builder/queryBuilder";
+import { FacultyModel } from "../faculty/faculty.model";
+import { FacultySearchableFields } from "./member.constant";
+
+export class FacultymemberServices {
+    static async getAllFacultyMembers(query: Record<string, unknown>) {
+        const facultyQuery = new QueryBuilder(
+            FacultyModel.find().populate('academicDepartment'),
+            query,
+        )
+            .search(FacultySearchableFields)
+            .filter()
+            .sort()
+            .paginate()
+            .fields();
+
+        const result = facultyQuery.getQuery;
+        return result;
+
+    }
+}
