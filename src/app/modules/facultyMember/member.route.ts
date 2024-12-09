b30f3cf16fe08aee07319ac0ee3fe2ec037c5c23
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-09 19:00:41 +0600

    [add + mod] : get all faculty member route added in member.route.ts

--- src/app/modules/facultyMember/member.route.ts
+++ src/app/modules/facultyMember/member.route.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { FacultyControllers } from "../faculty/faculty.controller";
 
 const FacultyMemberRouter: Router = Router();
+
+
+FacultyMemberRouter.get('/', FacultyControllers.getAllFaculties);
+
 
 export default FacultyMemberRouter;
