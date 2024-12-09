c58e66b14b3942df5cfbc613f05dbb61d9653a10
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-09 13:30:58 +0600

    [add + mod] : get  single faculty member controller added in member.controller.ts

--- src/app/modules/facultyMember/member.controller.ts
+++ src/app/modules/facultyMember/member.controller.ts
@@ -7,7 +7,25 @@ export class FacultyMemberControllers {
 
     static getAllFacultyMembers = catchAsync(async (req: Request, res: Response) => {
         const result = await FacultymemberServices.getAllFacultyMembers(req.query);
-        
+
         sendResponse(res,200,true,"Faculty members are retrieved successfully",result);
     });
+    
+    static getSingleFacultyMember = catchAsync(async (req, res) => {
+        const result = await FacultymemberServices.getSingleFacultyMember(req.params.id);
+        
+        sendResponse(res,200,true,"Faculty member is retrieved successfully",result);
+        
+    });
+
+
+
+
+
+
+
+
+
+
+
 }
