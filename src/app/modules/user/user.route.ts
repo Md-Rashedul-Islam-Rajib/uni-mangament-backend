bb3f184ef7bebdbebe80b20d8eae11c82922a8f0
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-09 19:07:59 +0600

    [add + mod] : create faculty member route added in user.route.ts

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { UserControllers } from './user.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { createStudentValidationSchema } from '../student/student.zodSchema';
+import { createFacultyMemberValidationSchema } from '../facultyMember/member.zodSchema';
 
 const UserRouter: Router = Router();
 
@@ -10,5 +11,8 @@ UserRouter.post(
     validateRequest(createStudentValidationSchema),
     UserControllers.createStudent,
 );
+
+
+UserRouter.post('/create-faculty-member',validateRequest(createFacultyMemberValidationSchema),UserControllers.createFacultyMember);
 
 export default UserRouter;
