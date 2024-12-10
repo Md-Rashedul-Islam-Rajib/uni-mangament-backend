aef0b875217ea52995dd2070c881ce79d477cb4b
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-10 21:33:52 +0600

    [add + mod] : create course service added in course.service.ts

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,3 +1,9 @@
+import { CourseModel } from "./course.model";
+import { TCourse } from "./course.types";
+
 export class CourseServices {
-    
+    static async createCourse(payload: TCourse) {
+        const result = await CourseModel.create(payload);
+        return result;
+    };
 }
