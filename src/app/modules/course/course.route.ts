ae89368376df9b661913e56116691093442a6cb3
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-10 23:46:40 +0600

    [add + mod] : get single course route added in course.route.ts

--- src/app/modules/course/course.route.ts
+++ src/app/modules/course/course.route.ts
@@ -8,6 +8,8 @@ const CourseRouter: Router = Router();
 
 CourseRouter.post('/create-course', validateRequest(createCourseValidationSchema), CourseServices.createCourse);
 
-CourseRouter.get("/",CourseControllers.getAllCourses);
+CourseRouter.get("/", CourseControllers.getAllCourses);
+
+CourseRouter.post("/:id", CourseServices.getSingleCourse);
 
 export default CourseRouter;
