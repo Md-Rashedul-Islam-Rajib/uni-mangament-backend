22b004541b0bf69ef9417d15e36159ad0a00390e
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-10 18:23:05 +0600

    [add + mod] : get single admin controller added in admin.controller.ts

--- src/app/modules/admin/admin.controller.ts
+++ src/app/modules/admin/admin.controller.ts
@@ -8,4 +8,13 @@ export class AdminControllers {
         const result = await AdminServices.getAllAdmin(req.query);
         sendResponse(res,200,true,"admins are retrieved successfully",result);
     });
+
+
+
+    static getSingleAdmin = catchAsync(async (req, res) => {
+        const result = await AdminServices.getSingleAdmin(req.params.id);
+        sendResponse(res,200,true,"admin is retrieved successfully", result);
+    });
+
+
 }
