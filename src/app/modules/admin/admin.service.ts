1f64d53c1fbc6b4109d92cb821f06316d02b9b66
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-10 12:10:19 +0600

    [add+ mod] : update admin added in admin.service.ts

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,10 +1,11 @@
 import QueryBuilder from "../../builder/queryBuilder";
 import { AdminSearchableFields } from "./admin.constant";
 import { AdminModel } from "./admin.model";
+import { TAdmin } from "./admin.types";
 
 export class AdminServices {
 
-    static async getAllAdmin(query: Record<string,unknown>) {
+    static async getAllAdmin(query: Record<string, unknown>) {
         const adminQuery = new QueryBuilder(AdminModel.find(), query)
             .search(AdminSearchableFields)
             .filter()
@@ -12,7 +13,7 @@ export class AdminServices {
             .paginate()
             .fields();
 
-        const result = await adminQuery.getQuery;
+        const result = adminQuery.getQuery;
         return result;
     }
 
@@ -22,6 +23,24 @@ export class AdminServices {
         return result;
     };
 
+    static async updateAdmin(id: string, payload: Partial<TAdmin>) {
+        const { name, ...remainingAdminData } = payload;
 
+        const modifiedUpdatedData = {
+            ...remainingAdminData,
+            ...(name &&
+                Object.keys(name).reduce(
+                    (acc, key) => ({
+                        ...acc,
+                        [`name.${key}`]: name[key as keyof typeof name],
+                    }),
+                    {},
+                )),
+        };
+        
 
+        const result = await AdminModel.findByIdAndUpdate({ id }, modifiedUpdatedData, { new: true, runValidators: true });
+        return result;
+
+    }
 }
