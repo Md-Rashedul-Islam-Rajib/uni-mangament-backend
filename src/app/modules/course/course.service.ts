d1f91e9ece7b40c3d4d11904836be7d396ae43d5
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-10 21:45:18 +0600

    [add + mod] : get all course service added in course.service.ts

--- src/app/modules/course/course.service.ts
+++ src/app/modules/course/course.service.ts
@@ -1,3 +1,5 @@
+import QueryBuilder from "../../builder/queryBuilder";
+import { CourseSearchableFields } from "./course.constant";
 import { CourseModel } from "./course.model";
 import { TCourse } from "./course.types";
 
@@ -6,4 +8,34 @@ export class CourseServices {
         const result = await CourseModel.create(payload);
         return result;
     };
+
+
+    static async getAllCourse(query: Record<string, unknown>) {
+        const courseQuery = new QueryBuilder(
+            CourseModel.find(),
+            query,
+        )
+            .search(CourseSearchableFields)
+            .filter()
+            .sort()
+            .paginate()
+            .fields();
+
+        const result = courseQuery.getQuery;
+        return result;
+    };
+
+
+    static async getSingleCourse(id: string) {
+        const result = await CourseModel.findById(id).populate("preRequisiteCourses.course");
+        return result;
+    };
+
+
+
+
+
+
+
+
 }
