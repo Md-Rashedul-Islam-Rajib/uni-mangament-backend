e7c71ed9e662c3634e2c11974d45a351cc22d463
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-10 21:15:56 +0600

    [add] : course.zodSchema.ts added

---
+++ src/app/modules/course/course.zodSchema.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+
+export const PreRequisiteCourseValidationSchema = z.object({
+    course: z.string(),
+    isDeleted: z.boolean().optional(),
+});
+
+export const createCourseValidationSchema = z.object({
+        title: z.string(),
+        prefix: z.string(),
+        code: z.number(),
+        credits: z.number(),
+        preRequisiteCourses: z
+            .array(PreRequisiteCourseValidationSchema)
+            .optional(),
+        isDeleted: z.boolean().optional(),
+});
+
+export const updatePreRequisiteCourseValidationSchema = z.object({
+    course: z.string(),
+    isDeleted: z.boolean().optional(),
+});
+
+export const updateCourseValidationSchema = z.object({
+        title: z.string().optional(),
+        prefix: z.string().optional(),
+        code: z.number().optional(),
+        credits: z.number().optional(),
+        preRequisiteCourses: z
+            .array(updatePreRequisiteCourseValidationSchema)
+            .optional(),
+        isDeleted: z.boolean().optional(),
+});
+
+export const facultiesWithCourseValidationSchema = z.object({
+        faculties: z.array(z.string()),
+});
