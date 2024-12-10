97c9ab88d1749ffcc85f784f0474a095deeaba5b
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-10 19:41:22 +0600

    [add + mod] :update admin route added in admin.route.ts

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { AdminControllers } from "./admin.controller";
+import validateRequest from "../../middlewares/validateRequest";
+import { updateAdminValidationSchema } from "./admin.zodSchema";
 
 const AdminRouter: Router = Router();
 
@@ -7,6 +9,6 @@ AdminRouter.get('/' , AdminControllers.getAllAdmins);
 
 AdminRouter.get('/:id' , AdminControllers.getSingleAdmin);
 
-
+AdminRouter.patch('/:id',validateRequest(updateAdminValidationSchema),AdminControllers.updateAdmin);
 
 export default AdminRouter;
