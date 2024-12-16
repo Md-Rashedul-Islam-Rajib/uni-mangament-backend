dc33bbd66de385ea8bf6d30320749af182813b37
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-16 20:01:33 +0600

    [add] : offeredCourse.service.ts added

---
+++ src/app/modules/offeredCourses/offeredCourse.service.ts
@@ -0,0 +1,36 @@
+import { TOfferedCourse } from "./offeredCourse.types";
+
+export class OfferedCourseServices {
+
+    static async createOfferedCourse(payload: TOfferedCourse) {
+        
+        const {
+            semesterRegistration,
+            academicFaculty,
+            academicDepartment,
+            course,
+            section,
+            faculty,
+            days,
+            startTime,
+            endTime,
+        } = payload;
+
+        const isRegSemesterExists = await Seme
+
+
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
