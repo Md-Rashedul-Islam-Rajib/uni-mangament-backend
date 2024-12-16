f106d38be5d048092227534525d8ad3b89a41a8f
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-17 01:41:29 +0600

    [add + mod] : get single regSemester controller added in regSemester.controller.ts

--- src/app/modules/SemesterRegistration/regSemester.controller.ts
+++ src/app/modules/SemesterRegistration/regSemester.controller.ts
@@ -1,4 +1,4 @@
-import catchAsync from "../../utilities/catchAsyncFn"
+import catchAsync from "../../utilities/catchAsyncFn";
 import sendResponse from "../../utilities/sendResponse";
 import { RegSemesterServices } from "./regSemester.service";
 
@@ -11,6 +11,11 @@ export class RegSemesterControllers {
 
     static getAllRegSemester = catchAsync(async (req, res) => {
         const result = await RegSemesterServices.getAllRegSemesters(req.query);
+        sendResponse(res,200,true,'Semester Registration is retrieved successfully',result);
+    });
+
+    static getSingleRegSemester = catchAsync(async (req, res) => {
+        const result = await RegSemesterServices.getSingleRegSemester(req.params.id);
         sendResponse(res,200,true,'Semester Registration is retrieved successfully',result);
     });
 
