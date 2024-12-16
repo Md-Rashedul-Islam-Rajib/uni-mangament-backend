8a212442d48b676d29cb6da193aae4771f690c83
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-17 01:34:29 +0600

    [add + mod] : create regSemester controller added in regSemester.controller.ts

--- src/app/modules/SemesterRegistration/regSemester.controller.ts
+++ src/app/modules/SemesterRegistration/regSemester.controller.ts
@@ -1,9 +1,12 @@
 import catchAsync from "../../utilities/catchAsyncFn"
+import sendResponse from "../../utilities/sendResponse";
+import { RegSemesterServices } from "./regSemester.service";
 
 export class RegSemesterControllers {
 
     static createRegSemester = catchAsync(async (req, res) => {
-        const re
+        const result = await RegSemesterServices.createRegSemester(req.body);
+        sendResponse(res, 201, true, "semester registration is created successfully", result);
     });
 
 }
