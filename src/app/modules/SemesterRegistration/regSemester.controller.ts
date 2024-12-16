57a4046a830971364084fe355bad31c7aecbb9c0
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-17 01:38:27 +0600

    [add + mod] : get all regSemester controller added in regSemester.controller.ts

--- src/app/modules/SemesterRegistration/regSemester.controller.ts
+++ src/app/modules/SemesterRegistration/regSemester.controller.ts
@@ -9,4 +9,9 @@ export class RegSemesterControllers {
         sendResponse(res, 201, true, "semester registration is created successfully", result);
     });
 
+    static getAllRegSemester = catchAsync(async (req, res) => {
+        const result = await RegSemesterServices.getAllRegSemesters(req.query);
+        sendResponse(res,200,true,'Semester Registration is retrieved successfully',result);
+    });
+
 }
