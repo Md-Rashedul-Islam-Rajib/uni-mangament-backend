4abb6b94708bdf9b133cc4cefe688a9aa21deee5
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-16 20:09:15 +0600

    [add] : regSemester.model.ts added

---
+++ src/app/modules/regSemester/regSemester.model.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+import { TRegSemester } from "./regSemester.types";
+import { SemesterRegistrationStatus } from "./regSemester.constant";
+
+const regSemesterSchema = new mongoose.Schema<TRegSemester>(
+    {
+        academicSemester: {
+            type: Schema.Types.ObjectId,
+            required: true,
+            unique: true,
+            ref: 'Semester',
+        },
+        status: {
+            type: String,
+            enum: SemesterRegistrationStatus,
+            default: 'UPCOMING',
+        },
+        startDate: {
+            type: Date,
+            required: true,
+        },
+        endDate: {
+            type: Date,
+            required: true,
+        },
+        minCredit: {
+            type: Number,
+            default: 3,
+        },
+        maxCredit: {
+            type: Number,
+            default: 15,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+export const RegSemesterModel = mongoose.model<TRegSemester>(
+    'RegSemester',
+    regSemesterSchema,
+);
