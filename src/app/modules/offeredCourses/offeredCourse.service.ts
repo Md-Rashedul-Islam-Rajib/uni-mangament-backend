1d98490f02d4d0c10384111fbbeb06dadab94b2c
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-17 12:49:10 +0600

    [add + mod] : update Offered course service added in offeredCourse.service.ts

--- src/app/modules/offeredCourses/offeredCourse.service.ts
+++ src/app/modules/offeredCourses/offeredCourse.service.ts
@@ -43,11 +43,12 @@ export class OfferedCourseServices {
         }
 
         const isCourseExists = await CourseModel.findById(course);
-        
+
         if (!isCourseExists) {
             throw new Error('course not found');
         }
-        const isFacultyMemberExists = await FacultyMemberModel.findById(faculty);
+        const isFacultyMemberExists =
+            await FacultyMemberModel.findById(faculty);
 
         if (!isFacultyMemberExists) {
             throw new Error('faculty member not found');
@@ -55,11 +56,13 @@ export class OfferedCourseServices {
 
         const isDeptBelongToFaculty = await DepartmentModel.findOne({
             _id: academicDepartment,
-            academicFaculty
+            academicFaculty,
         });
 
         if (!isDeptBelongToFaculty) {
-            throw new Error(`This ${isDeptExists.name} is not belong to this ${isFacultyExists.name}`);
+            throw new Error(
+                `This ${isDeptExists.name} is not belong to this ${isFacultyExists.name}`,
+            );
         }
 
         const isSameOfferedCourseExistsWithSameRegisteredSemesterWithSameSection =
@@ -76,7 +79,6 @@ export class OfferedCourseServices {
                 `Offered course with same section is already exist!`,
             );
         }
-
 
         const assignedSchedules = await OfferedCourseModel.find({
             semesterRegistration,
@@ -96,13 +98,11 @@ export class OfferedCourseServices {
             );
         }
 
-
         const result = await OfferedCourseModel.create({
-            ...payload
+            ...payload,
         });
         return result;
     }
-
 
     static async getAllOfferedCourses(query: Record<string, unknown>) {
         const offeredCourseQuery = new QueryBuilder(
@@ -113,11 +113,10 @@ export class OfferedCourseServices {
             .sort()
             .paginate()
             .fields();
-        
+
         const result = offeredCourseQuery.getQuery;
         return result;
-    };
-
+    }
 
     static async getSingleOfferedCourse(id: string) {
         const offeredCourse = await OfferedCourseModel.findById(id);
@@ -127,33 +126,70 @@ export class OfferedCourseServices {
         }
 
         return offeredCourse;
+    }
+
+    static async updateOfferedCourse(
+        id: string,
+        payload: Pick<
+            TOfferedCourse,
+            'faculty' | 'days' | 'startTime' | 'endTime'
+        >,
+    ) {
+        const { faculty, days, startTime, endTime } = payload;
+
+        const isOfferedCourseExists = await OfferedCourseModel.findById(id);
+
+        if (!isOfferedCourseExists) {
+            throw new Error(
+                'Offered course not found !',
+            );
+        }
+
+        const isFacultyExists = await FacultyMemberModel.findById(faculty);
+
+        if (!isFacultyExists) {
+            throw new Error('Faculty member not found !');
+        }
+
+        const semesterRegistration = isOfferedCourseExists.semesterRegistration;
+        // get the schedules of the faculties
+
+        // Checking the status of the semester registration
+        const semesterRegistrationStatus =
+            await RegSemesterModel.findById(semesterRegistration);
+
+        if (semesterRegistrationStatus?.status !== 'UPCOMING') {
+            throw new Error(
+                `You can not update this offered course as it is ${semesterRegistrationStatus?.status}`,
+            );
+        }
+
+        // check if the faculty is available at that time.
+        const assignedSchedules = await OfferedCourseModel.find({
+            semesterRegistration,
+            faculty,
+            days: { $in: days },
+        }).select('days startTime endTime');
+
+        const newSchedule = {
+            days,
+            startTime,
+            endTime,
+        };
+
+        if (hasTimeConflict(assignedSchedules, newSchedule)) {
+            throw new Error(
+                `This faculty is not available at that time ! Choose other time or day`,
+            );
+        }
+
+        const result = await OfferedCourseModel.findByIdAndUpdate(id, payload, {
+            new: true,
+        });
+        return result;
     };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    sta
 
 
 
