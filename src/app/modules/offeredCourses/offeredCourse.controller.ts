42e204187b852d57b8c47398539b626f8bee1b3b
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-17 13:02:10 +0600

    [add + mod] : create Offered course controller added in offeredCourse.controller.ts

--- src/app/modules/offeredCourses/offeredCourse.controller.ts
+++ src/app/modules/offeredCourses/offeredCourse.controller.ts
@@ -3,7 +3,7 @@ import sendResponse from "../../utilities/sendResponse";
 import { OfferedCourseServices } from "./offeredCourse.service";
 
 
-export class OfferedCourseControllers 
+export class OfferedCourseControllers {
     static createOfferedCourse = catchAsync(async (req, res) => {
         const result = await OfferedCourseServices.createOfferedCourse(req.body);
         sendResponse(res, 201, true, "offered course is created successfully", result);
