1451d093a8b6d8afcd38916f09fb03eb8aad7cdc
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-18 17:46:51 +0600

    [mod] : env configuration modified

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -10,4 +10,8 @@ export default {
     db_url: process.env.DB_URL as string,
     default_password: process.env.DEFAULT_PASSWORD as string,
     bcrypt_salt_rounds: process.env.SALT_ROUND as string,
+    jwt_access_secret: process.env.JWT_ACCESS_SECRET as string,
+    jwt_refresh_secret: process.env.JWT_REFRESH_SECRET as string,
+    jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN as string,
+    jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN as string,
 };
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,11 +1,28 @@
+import { JwtPayload } from 'jsonwebtoken';
 import { UserModel } from "../user/user.model";
 import { TLoginUser } from "./auth.types";
+import { createToken, preValidatingUser } from "./auth.utilities";
 
 export class AuthServices {
 
     static async loginUser(payload: TLoginUser) {
-        
+        const user = await preValidatingUser(payload.id);
 
+        const passwordMatched = await UserModel.isPasswordMatched(payload?.password, user?.password);
+        
+        if (!passwordMatched) {
+            throw new Error('password did not matched');
+        }
+
+        const JwtPayload = {
+            userId: user?.id,
+            role: user?.role
+        };
+
+        const accessToken = createToken(
+            JwtPayload,
+            conf
+        );
         
     };
 
