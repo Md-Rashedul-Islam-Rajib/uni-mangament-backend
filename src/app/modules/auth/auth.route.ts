b89ff594296dd6f1c3252796cb2923034523ee34
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-18 19:15:27 +0600

    [add + mod] : change password route added in auth.route.ts

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import validateRequest from "../../middlewares/validateRequest";
-import { loginValidationSchema } from "./auth.zodSchema";
+import { changePasswordValidationSchema, loginValidationSchema } from "./auth.zodSchema";
 import { AuthControllers } from "./auth.controller";
+import auth from "../../middlewares/auth";
+import { USER_ROLE } from "../user/user.constants";
 
 const AuthRouter: Router = Router();
 
-AuthRouter.post('/login',validateRequest(loginValidationSchema),AuthControllers.loginUser);
+AuthRouter.post('/login', validateRequest(loginValidationSchema), AuthControllers.loginUser);
+
+AuthRouter.post('/change-password', auth(
+    USER_ROLE.admin,
+    USER_ROLE.faculty,
+    USER_ROLE.student
+    
+),validateRequest(changePasswordValidationSchema),AuthControllers.changePassword);
 
 export default AuthRouter;
