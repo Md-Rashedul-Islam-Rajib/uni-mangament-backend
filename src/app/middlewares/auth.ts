8514b7db1c47352e9b332205173a9a23c18747c2
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-18 19:11:06 +0600

    [add ] :  auth middleware added in auth.ts

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,37 @@
+import jwt,{ JwtPayload } from 'jsonwebtoken';
+import { TUserRole } from "../modules/user/user.types";
+import catchAsync from "../utilities/catchAsyncFn";
+import config from '../config';
+import { preValidatingUser } from '../modules/auth/auth.utilities';
+
+const auth = (...roles: TUserRole[]) => {
+    return catchAsync(async (req, res, next) => {
+        const token = req.headers.authorization;
+
+        // checking if the token is missing
+        if (!token) {
+            throw new Error(
+                'You are not authorized!'
+            );
+        }
+
+        // checking if the given token is valid
+        const decoded = jwt.verify(
+            token,
+            config.jwt_access_secret as string,
+        ) as JwtPayload;
+
+        const { role, userId, iat } = decoded;
+
+        const user = await preValidatingUser(userId, iat);
+        
+        if (roles && !roles.includes(role)) {
+            throw new Error('you are not authorized');
+        }
+
+        req.user = decoded;
+        next();
+    });
+};
+
+export default auth;
