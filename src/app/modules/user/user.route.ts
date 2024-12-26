af794688af86b9c9fa50dd91044d8d748f4b6f87
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-26 14:53:25 +0600

    [add + mod] :  change status route added in user.route.ts

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -5,27 +5,38 @@ import { createStudentValidationSchema } from '../student/student.zodSchema';
 import { createFacultyMemberValidationSchema } from '../facultyMember/member.zodSchema';
 import { createAdminValidationSchema } from '../admin/admin.zodSchema';
 import { upload } from '../../utilities/cloudinaryHandler';
+import { parsingRequest } from '../../middlewares/parsingRequest';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.constants';
+import { changeStatusValidationSchema } from './user.zodschema';
 
 const UserRouter: Router = Router();
 
 UserRouter.post(
     '/create-student',
     upload.single('file'),
-    ,
+    parsingRequest,
     validateRequest(createStudentValidationSchema),
     UserControllers.createStudent,
 );
 
 UserRouter.post(
     '/create-faculty-member',
+    auth(USER_ROLE.admin),
+    upload.single('file'),
+    parsingRequest,
     validateRequest(createFacultyMemberValidationSchema),
     UserControllers.createFacultyMember,
 );
 
 UserRouter.post(
     '/create-admin',
+    upload.single('file'),
+    parsingRequest,
     validateRequest(createAdminValidationSchema),
     UserControllers.createAdmin,
 );
+
+UserRouter.post('/change-status/:id', auth(USER_ROLE.admin),validateRequest(changeStatusValidationSchema), UserControllers.changeStatus);
 
 export default UserRouter;
