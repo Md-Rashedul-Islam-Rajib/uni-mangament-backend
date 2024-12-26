be1ee847a6bf91742a206d72d8c4a8090c760573
Md Rashedul Islam <rajib5570@gmail.com>
2024-12-26 13:17:00 +0600

    [add + mod] :  user status constant added in user.constant.ts

--- dist/app/modules/faculty/faculty.zodSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateFacultySchema = exports.facultyCreationSchema = void 0;
-const zod_1 = require("zod");
-exports.facultyCreationSchema = zod_1.z.object({
-    name: zod_1.z.string({ message: 'Name for faculty is required!' }).min(2),
-});
-exports.updateFacultySchema = exports.facultyCreationSchema.partial();
